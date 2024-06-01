9679ad57cd6e35c1708dc1a06db92a51d1695d8b
Jeonhanjoon <166356744+Hanjun2022@users.noreply.github.com>
2024-06-01 13:19:24 +0900

    Hi
    HI

---
+++ kp/src/AppRouter.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {BrowserRouter,Routes,Route} from 'react-route-dom';
+import Add from './Add';
+import App from './App';
+
+function AppRouter(){
+    return (
+        <BrowserRouter>
+   
+            <Routes>
+                <Route path='/' element={<App/>}/>
+                <Route path='/create' element={<Add/>}/>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default AppRouter;
